7fc04e6ecc1b894fb89dc9b29d10900744f760bd
VexAve <designdyle@gmail.com>
2025-12-17 16:12:21 +0700

    Add button for choices

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const toTitleCase = string => {
+    return string[0].toUpperCase() + string.slice(1);
+}
+
+console.log("hello");
+
 const getComputerChoice = () => {
     const choice = Math.floor(Math.random() * 3);
 
@@ -10,38 +16,34 @@ const getComputerChoice = () => {
     }
 }
 
-const getHumanChoice = () => {
-    return prompt();
-}
+const choices = document.getElementById("choices");
 
-const toTitleCase = string => {
-    return string[0].toUpperCase() + string.slice(1);
-}
-
-const playGame = () => {
-    let humanScore = 0;
-    let computerScore = 0;
-
-    const playRound = (humanChoice, computerChoice) => {
-        humanChoice = humanChoice.toLowerCase();
-        if (
-            (humanChoice === "rock" && computerChoice == "scissors") ||
-            (humanChoice === "scissors" && computerChoice == "paper") ||
-            (humanChoice === "paper" && computerChoice == "rock")
-        ) {
-            console.log("You win! " + humanChoice.toTitleCase() + " beats " + computerChoice.toTitleCase() + ".\n");
-            humanScore++;
-        } else if (humanChoice === computerChoice) {
-            console.log("It's a tie!\n");
-        } else {
-            console.log("You lose! " + computerChoice.toTitleCase() + " beats " + humanChoice.toTitleCase() + ".\n");
-        }
-    }
-
-    for (let i = 0; i < 5; i++) {
-        const humanChoice = getHumanChoice();
+choices.addEventListener("click", event => {
+    console.log("hello");
+    if (event.target.tagName === "BUTTON") {
         const computerChoice = getComputerChoice();
+        const humanChoice = event.target.id;
         playRound(humanChoice, computerChoice);
-        console.log(`Human: ${humanScore}\nComputer: ${computerScore}`);
-    }    
+    }
+});
+
+let humanScore = 0;
+let computerScore = 0;
+
+
+const playRound = (humanChoice, computerChoice) => {
+    humanChoice = humanChoice.toLowerCase();
+    if (
+        (humanChoice === "rock" && computerChoice == "scissors") ||
+        (humanChoice === "scissors" && computerChoice == "paper") ||
+        (humanChoice === "paper" && computerChoice == "rock")
+    ) {
+        console.log("You win! " + humanChoice.toTitleCase() + " beats " + computerChoice.toTitleCase() + ".\n");
+        humanScore++;
+    } else if (humanChoice === computerChoice) {
+        console.log("It's a tie!\n");
+    } else {
+        console.log("You lose! " + computerChoice.toTitleCase() + " beats " + humanChoice.toTitleCase() + ".\n");
+    }
 }
+
