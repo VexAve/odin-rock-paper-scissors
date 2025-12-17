2f9da2b14df5b4216d5ebb816c58d70bcc5700b1
VexAve <designdyle@gmail.com>
2025-12-17 13:28:34 +0700

    Add playGame function

--- script.js
+++ script.js
@@ -18,21 +18,30 @@ const toTitleCase = string => {
     return string[0].toUpperCase() + string.slice(1);
 }
 
-let humanScore = 0;
-let computerScore = 0;
+const playGame = () => {
+    let humanScore = 0;
+    let computerScore = 0;
 
-const playRound = (humanChoice, computerChoice) => {
-    humanChoice = humanChoice.toLowerCase();
-    if (
-        (humanChoice === "rock" && computerChoice == "scissors") ||
-        (humanChoice === "scissors" && computerChoice == "paper") ||
-        (humanChoice === "paper" && computerChoice == "rock")
-    ) {
-        console.log("You win! " + humanChoice.toTitleCase() + " beats " + computerChoice.toTitleCase() + ".");
-        humanScore++;
-    } else if (humanChoice === computerChoice) {
-        console.log("It's a tie!");
-    } else {
-        console.log("You lose! " + computerChoice.toTitleCase() + " beats " + humanChoice.toTitleCase() + ".");
+    const playRound = (humanChoice, computerChoice) => {
+        humanChoice = humanChoice.toLowerCase();
+        if (
+            (humanChoice === "rock" && computerChoice == "scissors") ||
+            (humanChoice === "scissors" && computerChoice == "paper") ||
+            (humanChoice === "paper" && computerChoice == "rock")
+        ) {
+            console.log("You win! " + humanChoice.toTitleCase() + " beats " + computerChoice.toTitleCase() + ".\n");
+            humanScore++;
+        } else if (humanChoice === computerChoice) {
+            console.log("It's a tie!\n");
+        } else {
+            console.log("You lose! " + computerChoice.toTitleCase() + " beats " + humanChoice.toTitleCase() + ".\n");
+        }
     }
+
+    for (let i = 0; i < 5; i++) {
+        const humanChoice = getHumanChoice();
+        const computerChoice = getComputerChoice();
+        playRound(humanChoice, computerChoice);
+        console.log(`Human: ${humanScore}\nComputer: ${computerScore}`);
+    }    
 }
