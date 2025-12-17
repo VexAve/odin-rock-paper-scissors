6323c5bf3d3787f32b00cc9739584bb867fbf3f0
VexAve <designdyle@gmail.com>
2025-12-17 13:06:18 +0700

    Add getHumanChoice function

--- script.js
+++ script.js
@@ -2,12 +2,17 @@ const getComputerChoice = () => {
     const choice = Math.floor(Math.random() * 3);
 
     if (choice === 0) {
-        return "Rock";
+        return "rock";
     } else if (choice === 1) {
-        return "Paper";
+        return "raper";
     } else {
-        return "Scissors";
+        return "scissors";
     }
 }
 
+const getHumanChoice = () => {
+    return prompt();
+}
+
 console.log(getComputerChoice());
+console.log(getHumanChoice());
